01306e47031020cc1bf2da7b2933f370538663b5
mirry18299 <141658211+mirry18299@users.noreply.github.com>
2023-10-16 11:01:39 +0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const router = express.Router();
+
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe", 
+        age: 25
+    },
+
+    {
+        firstName: "Jane",
+        lastName: "Doe", 
+        age: 24
+    }
+]
+
+router.get("/", (req, res) => {
+    res.send(users);
+});
+
+router.post("/", (req, res) => {
+    const user = req.body;
+
+    users.push(user)
+
+    res.send(`user with the name ${user.firstName} added to the database!`);
+});
+
+export default router;
